36b6c524edf4269e94096f752036f3ab932a2ece
Justin Fangrad <justin.fangrad@gmail.com>
2020-01-11 11:53:04 -0500

    First commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NavBar.scss';
+
+const NavBar = () => (
+  <div className="NavBar">
+
+  </div>
+);
+
+export default NavBar;
---
+++ src/sections/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './About.scss';
+
+const About = () => (
+  <div className="Section Section-Primary">
+
+  </div>
+);
+
+export default About;
