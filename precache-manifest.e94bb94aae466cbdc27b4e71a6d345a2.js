dac8d921cce227104200cc23fde6dc21c6e9c5aa
Justin Fangrad <justin.fangrad@gmail.com>
2020-01-12 21:11:45 -0500

    Updates

---
+++ precache-manifest.e94bb94aae466cbdc27b4e71a6d345a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f80ed364a6b288462927097a508c897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a331ec52778ba3c76a2",
+    "url": "/static/css/main.a10535d6.chunk.css"
+  },
+  {
+    "revision": "7011361b2f06029a577f",
+    "url": "/static/js/2.81358313.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.81358313.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a331ec52778ba3c76a2",
+    "url": "/static/js/main.1de1b93b.chunk.js"
+  },
+  {
+    "revision": "a796a236ee8470d38315",
+    "url": "/static/js/runtime-main.783779e9.js"
+  },
+  {
+    "revision": "d78f60c7838b15b677fba6b575d13f4e",
+    "url": "/static/media/frontend.d78f60c7.svg"
+  },
+  {
+    "revision": "6735cbcc28bdfd79b4365a85958d9802",
+    "url": "/static/media/justin-image-cropped.6735cbcc.png"
+  },
+  {
+    "revision": "a19b0ba01ce8511c4c090793d8e4f6ee",
+    "url": "/static/media/programming.a19b0ba0.svg"
+  }
+]);
