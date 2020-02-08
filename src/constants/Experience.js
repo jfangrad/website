59ed49fa01992447b36d87f0a6ccb043b535ca2a
Justin Fangrad <justin.fangrad@gmail.com>
2020-02-08 11:11:13 -0500

    Remove unnecessary files. Add in last experience

--- src/constants/Experience.js
+++ src/constants/Experience.js
@@ -2,7 +2,7 @@ export const Experiences = [
   {
     companyName: 'Sobol',
     companyLink: 'https://sobol.io/',
-    title: 'Full Stack Developer',
+    title: 'Full Stack Developer Intern',
     date: 'May 2019 - Aug 2019',
     description: [
       'End to end full feature development utilizing JavaScript, React, Redux, Express, MongoDb, and Neo4J.',
@@ -12,7 +12,7 @@ export const Experiences = [
   {
     companyName: 'Shopify',
     companyLink: 'https://www.shopify.ca/',
-    title: 'Front End Developer',
+    title: 'Front End Developer Intern',
     date: 'Sep 2018 - Dec 2018',
     description: [
       'Worked as a part of the Shopify App Store team and used tools including TypeScript, React, and GraphQL.',
@@ -22,11 +22,21 @@ export const Experiences = [
   {
     companyName: 'Vidyard',
     companyLink: 'https://www.vidyard.com/',
-    title: 'Software Developer',
+    title: 'Software Developer co-op',
     date: 'Jan 2018 - Apr 2018',
     description: [
       'Worked on the Vidyard GoVideo chrome extension and embedded web application used for recording, sharing, and managing videos.',
       'Focused on a full application UX/UI redesign and application structure using JavaScript React, and Redux'
+    ],
+  },
+  {
+    companyName: 'Vidyard',
+    companyLink: 'https://www.vidyard.com/',
+    title: 'Systems Analyst co-op',
+    date: 'Jan 2017 - Apr 2017',
+    description: [
+      'Worked in the IT department and assisted in onboarding new employees and ensuring their technology needs were met',
+      'Focused on setting up new laptops and creating a helpdesk website for internal company use.'
     ],
   },
 ];
--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -5,22 +5,32 @@ import { GroupedExperiences } from '../constants/Experience';
 import './Experience.scss';
 import ScrollMarker from '../components/ScrollMarker';
 
-const ExperienceItem = ({ companyName, companyLink, title, date, description, onRight, shouldRenderDivider }) => {
+const ExperienceItem = ({ companyName, companyLink, title, date, description, onRight, shouldRenderDivider, isLast }) => {
   const mappedDescriptions = description.map(d => <div className="ExperienceItem-description-item" key={d}>{d}</div>);
   const className = classNames('ExperienceItem', { 'ExperienceItem-on-right': onRight});
-  const dividerClassName = classNames('ExperienceItem-divider', {'ExperienceItem-divider-right': onRight});
+  const dividerClassName = classNames(
+    'ExperienceItem-divider',
+    {
+      'ExperienceItem-divider-right': onRight,
+      'ExperienceItem-divider-show': shouldRenderDivider,
+      'ExperienceItem-divider-last': isLast,
+    },
+  );
+
   return (
     <div className={className}>
-      <div className="ExperienceItem-header">
-        <a href={companyLink} className='ExperienceItem-company-name' target="_blank" rel="noopener noreferrer">
-          {companyName}
-          {companyName && <ExternalLink />}
-        </a>
-        <div className="ExperienceItem-date">{date}</div>
+      <div className="ExperienceItem-content">
+        <div className="ExperienceItem-header">
+          <a href={companyLink} className='ExperienceItem-company-name' target="_blank" rel="noopener noreferrer">
+            {companyName}
+            {companyName && <ExternalLink />}
+          </a>
+          <div className="ExperienceItem-date">{date}</div>
+        </div>
+        <div className="ExperienceItem-job-title">{title}</div>
+        <div className="ExperienceItem-description">{mappedDescriptions}</div>
       </div>
-      <div className="ExperienceItem-job-title">{title}</div>
-      <div className="ExperienceItem-description">{mappedDescriptions}</div>
-      {shouldRenderDivider && <div className={dividerClassName} />}
+      <div className={dividerClassName} />
     </div>
   );
 };
@@ -35,6 +45,7 @@ const Experience = () => {
           <ExperienceItem
             {...item}
             onRight={index === 1}
+            isLast={index === group.length - 1 && !shouldRenderDivider}
             key={`${item.companyName} ${item.date}`}
             shouldRenderDivider={shouldRenderDivider}
           />,
